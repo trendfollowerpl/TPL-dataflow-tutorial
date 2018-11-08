1558cdbcf8eb564c7461e095bd7f65937365fdc6
Piotr Szlaski <piotr.szlaski@ivanti.com>
2018-11-08 13:58:11 +0100

    TransformBlockExample proj finished

--- TransformBlockExample/Program.cs
+++ TransformBlockExample/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace TransformBlockExample
 {
@@ -6,7 +8,27 @@ namespace TransformBlockExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var transformBlock = new TransformBlock<int, string>(i =>
+           {
+               Task.Delay(500).Wait();
+               return i.ToString();
+           }, new ExecutionDataflowBlockOptions() { MaxDegreeOfParallelism = 2 });
+
+            for (int i = 0; i < 10; i++)
+            {
+                transformBlock.Post(i);
+                Console.WriteLine($"Number of messages in the input queue: {transformBlock.InputCount}");
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Number of messages in the input queue: {transformBlock.OutputCount}");
+                var result = transformBlock.Receive();
+                Console.WriteLine($"Recived: {result}");
+            }
+
+            Console.WriteLine("done");
+            Console.ReadKey();
         }
     }
 }
