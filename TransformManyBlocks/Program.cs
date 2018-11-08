96b9051412736430244ba4697c7fb01dda7cc049
Piotr Szlaski <piotr.szlaski@ivanti.com>
2018-11-08 15:31:16 +0100

    TransformManyBlocks proj finish

---
+++ TransformManyBlocks/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks.Dataflow;
+
+namespace TransformManyBlocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var transformManyBlock = new TransformManyBlock<int, string>(
+                i => FindEvenNumbers(i),
+                new ExecutionDataflowBlockOptions() { MaxDegreeOfParallelism = 5 }
+                );
+            var printBlock = new ActionBlock<string>(a => Console.WriteLine($"Received message: {a}"));
+            DataflowBlock.LinkTo(transformManyBlock, printBlock);
+
+            for (int i = 0; i < 10; i++)
+            {
+                transformManyBlock.Post(i);
+            }
+
+            Console.WriteLine("done");
+            Console.ReadKey();
+        }
+
+        private static IEnumerable<string> FindEvenNumbers(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return $"{number}: {i}";
+                }
+            }
+        }
+    }
+}
