c6b2e4735b567a2ed034faa8190d859cf6a77871
Piotr Szlaski <piotr.szlaski@ivanti.com>
2018-11-09 08:54:32 +0100

    BroadcastBlockExample created

--- BroadcastBlockExample/Program.cs
+++ BroadcastBlockExample/Program.cs
@@ -2,20 +2,20 @@
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 
-namespace BufferBlockExample
+namespace BroadcastBlockExample
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var bufferBlock = new BufferBlock<int>(new DataflowBlockOptions(){BoundedCapacity = 1});
+            var bufferBlock = new BufferBlock<int>(new DataflowBlockOptions() {BoundedCapacity = 1});
             var a1 = new ActionBlock<int>(
                 a =>
                 {
                     Console.WriteLine($"Message {a} was processed by consumer 1");
                     Task.Delay(100).Wait();
                 },
-                new ExecutionDataflowBlockOptions() { BoundedCapacity = 1 });
+                new ExecutionDataflowBlockOptions() {BoundedCapacity = 1});
 
             var a2 = new ActionBlock<int>(
                 a =>
@@ -23,7 +23,7 @@ namespace BufferBlockExample
                     Console.WriteLine($"Message {a} was processed by consumer 2");
                     Task.Delay(100).Wait();
                 },
-                new ExecutionDataflowBlockOptions() { BoundedCapacity = 1 });
+                new ExecutionDataflowBlockOptions() {BoundedCapacity = 1});
 
             bufferBlock.LinkTo(a1);
             bufferBlock.LinkTo(a2);
