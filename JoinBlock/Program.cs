be3590dd9adff0280530d5bb2477c5eb19597e2c
Piotr Szlaski <piotr.szlaski@ivanti.com>
2018-11-09 11:03:36 +0100

    JoinBlock proj added

---
+++ JoinBlock/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace JoinBlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var broadcastBlock =
+                new BroadcastBlock<int>(a => a,
+                    new DataflowBlockOptions() { BoundedCapacity = 1 }
+                );
+
+            var a1 = new ActionBlock<int>(
+                a =>
+                {
+                    Console.WriteLine($"Message {a} was processed by consumer 1");
+                    Task.Delay(100).Wait();
+                });
+
+            var a2 = new ActionBlock<int>(
+                a =>
+                {
+                    Console.WriteLine($"Message {a} was processed by consumer 2");
+                    Task.Delay(100).Wait();
+                });
+
+            broadcastBlock.LinkTo(a1);
+            broadcastBlock.LinkTo(a2);
+
+            for (int i = 0; i < 10; i++)
+            {
+                var i1 = i;
+                broadcastBlock.SendAsync(i)
+                    .ContinueWith(a =>
+                    {
+                        Console.WriteLine(a.Result ? $"Messgae {i1} was accepted" : $"Messgae {i1} was rejected");
+                    });
+            }
+
+            Console.WriteLine("done");
+            Console.ReadLine();
+        }
+    }
+}
