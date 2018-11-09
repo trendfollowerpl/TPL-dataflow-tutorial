f3eab6908052e53ac0853621600a9ff0450f42cf
Piotr Szlaski <piotr.szlaski@ivanti.com>
2018-11-09 09:38:21 +0100

    BufferBlock finish

--- BroadcastBlockExample/Program.cs
+++ BroadcastBlockExample/Program.cs
@@ -8,30 +8,32 @@ namespace BroadcastBlockExample
     {
         static void Main(string[] args)
         {
-            var bufferBlock = new BufferBlock<int>(new DataflowBlockOptions() {BoundedCapacity = 1});
+            var broadcastBlock = 
+                new BroadcastBlock<int>(a => a,
+                    new DataflowBlockOptions() { BoundedCapacity = 1 }
+                    );
+
             var a1 = new ActionBlock<int>(
                 a =>
                 {
                     Console.WriteLine($"Message {a} was processed by consumer 1");
                     Task.Delay(100).Wait();
-                },
-                new ExecutionDataflowBlockOptions() {BoundedCapacity = 1});
+                });
 
             var a2 = new ActionBlock<int>(
                 a =>
                 {
                     Console.WriteLine($"Message {a} was processed by consumer 2");
                     Task.Delay(100).Wait();
-                },
-                new ExecutionDataflowBlockOptions() {BoundedCapacity = 1});
+                });
 
-            bufferBlock.LinkTo(a1);
-            bufferBlock.LinkTo(a2);
+            broadcastBlock.LinkTo(a1);
+            broadcastBlock.LinkTo(a2);
 
             for (int i = 0; i < 10; i++)
             {
                 var i1 = i;
-                bufferBlock.SendAsync(i)
+                broadcastBlock.SendAsync(i)
                     .ContinueWith(a =>
                     {
                         Console.WriteLine(a.Result ? $"Messgae {i1} was accepted" : $"Messgae {i1} was rejected");
