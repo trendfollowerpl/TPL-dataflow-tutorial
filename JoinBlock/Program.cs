48fc7b75720db9b5e82de845f1796be004606c43
Piotr Szlaski <piotr.szlaski@ivanti.com>
2018-11-09 15:02:58 +0100

    joinBlock finished

--- JoinBlock/Program.cs
+++ JoinBlock/Program.cs
@@ -17,17 +17,31 @@ namespace JoinBlock
                 a =>
                 {
                     Console.WriteLine($"Message {a} was processed by consumer 1");
-                    Task.Delay(100).Wait();
-                    return a;
-                });
+                    if (a % 2 == 0)
+                    {
+                        Task.Delay(300).Wait();
+                    }
+                    else
+                    {
+                        Task.Delay(50).Wait();
+                    }
+                    return a * -1;
+                }, new ExecutionDataflowBlockOptions() { MaxDegreeOfParallelism = 3 });
 
             var a2 = new TransformBlock<int, int>(
                 a =>
                 {
                     Console.WriteLine($"Message {a} was processed by consumer 2");
-                    Task.Delay(100).Wait();
+                    if (a % 2 != 0)
+                    {
+                        Task.Delay(300).Wait();
+                    }
+                    else
+                    {
+                        Task.Delay(50).Wait();
+                    }
                     return a;
-                });
+                }, new ExecutionDataflowBlockOptions() { MaxDegreeOfParallelism = 3 });
 
             broadcastBlock.LinkTo(a1);
             broadcastBlock.LinkTo(a2);
@@ -36,7 +50,9 @@ namespace JoinBlock
             a1.LinkTo(joinblock.Target1);
             a2.LinkTo(joinblock.Target2);
 
-            var printBlock = new ActionBlock<Tuple<int,int>>(a => Console.WriteLine($"Message {a} was processed"));
+            var printBlock = new ActionBlock<Tuple<int, int>>(
+                a => Console.WriteLine($"Message {a} was processed. Sum: {a.Item1 + a.Item2}")
+                );
             joinblock.LinkTo(printBlock);
 
             for (int i = 0; i < 10; i++)
