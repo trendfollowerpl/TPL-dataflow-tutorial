01ef81ccae30cb26211ba1df06391e495bcc2f76
trendfollowerpl <trendfollowerpl@windowslive.com>
2018-11-12 10:54:58 +0100

    MultipleProducersExample start

---
+++ MultipleProducersExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultipleProducersExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
