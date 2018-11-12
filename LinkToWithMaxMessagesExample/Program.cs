e8a06e92ed0d6518e0fa7088f1c04ce652bf1a44
trendfollowerpl <trendfollowerpl@windowslive.com>
2018-11-12 10:31:04 +0100

    LinkToWithMaxMessagesExample proj finished

---
+++ LinkToWithMaxMessagesExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace LinkToWithMaxMessagesExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var bufferBlock = new BufferBlock<int>(new DataflowBlockOptions() { BoundedCapacity = 1 });
+            var a1 = new ActionBlock<int>(
+                a =>
+                {
+                    Console.WriteLine($"Message {a} was processed by consumer 1");
+                    Task.Delay(100).Wait();
+                });
+
+            var a2 = new ActionBlock<int>(
+                a =>
+                {
+                    Console.WriteLine($"Message {a} was processed by consumer 2");
+                    Task.Delay(100).Wait();
+                });
+
+            bufferBlock.LinkTo(a1);
+            bufferBlock.LinkTo(a2, new DataflowLinkOptions { Append = false, MaxMessages = 5 });
+
+            for (int i = 0; i < 10; i++)
+            {
+                await bufferBlock.SendAsync(i);
+            }
+
+            Console.WriteLine("done");
+            Console.ReadLine();
+        }
+    }
+}
