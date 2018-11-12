0b0bbd70b6932e7d791878204ad436df8691d139
trendfollowerpl <trendfollowerpl@windowslive.com>
2018-11-12 11:29:14 +0100

    MultipleProducersExample example finish

--- MultipleProducersExample/Program.cs
+++ MultipleProducersExample/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using LinkToWithPropagation;
 
 namespace MultipleProducersExample
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var producer1 = new TransformBlock<string, string>(async n =>
+               {
+                   await Task.Delay(150);
+                   return n;
+               });
+
+            var producer2 = new TransformBlock<string, string>(n =>
+            {
+               // Task.Delay(500).Wait();
+                return n;
+            });
+
+            var printBlock = new ActionBlock<string>(n => Console.WriteLine(n));
+
+            producer1.LinkTo(printBlock);
+            producer2.LinkTo(printBlock);
+
+            for (int i = 0; i < 10; i++)
+            {
+                producer1.Post($"Producer 1 message: {i}");
+                producer2.Post($"Producer 2 message: {i}");
+            }
+
+            producer1.Complete();
+            producer2.Complete();
+            await Task.WhenAll(new[] { producer1.Completion, producer2.Completion })
+                .ContinueWith(_ => printBlock.Complete())
+                .ContinueWith(_ => printBlock.Completion);
+                
+            Console.WriteLine("done");
+            Console.ReadLine();
         }
     }
 }
