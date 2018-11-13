af16247f2ca8dab20fc7059e33669a3cd8a58381
Piotr Szlaski <piotr.szlaski@ivanti.com>
2018-11-13 15:20:14 +0100

    ErrorHandlingExample proj added

---
+++ ErrorHandlingExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+
+namespace ErrorHandlingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var block = new ActionBlock<int>(n =>
+            {
+                if (n==5)
+                {
+                    throw new ArgumentException("Sth went wrong");
+                }
+                Console.WriteLine($"Message {n} posted");
+            });
+
+            for (int i = 0; i < 10; i++)
+            {
+                block.Post(i);
+            }
+
+            Console.WriteLine("DONE");
+            Console.ReadLine();
+        }
+    }
+}
