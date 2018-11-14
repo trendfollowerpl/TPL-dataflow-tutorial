ac802ec5c0a08a67bcb04fc19fbf8c86dcafa649
Piotr Szlaski <piotr.szlaski@ivanti.com>
2018-11-14 14:51:24 +0100

    CustomBlockEncapsulate proj added

---
+++ CustomBlockEncapsulate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomBlockEncapsulate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
