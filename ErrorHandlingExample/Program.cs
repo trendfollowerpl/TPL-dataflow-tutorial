15222075588b29038ad5035aee95bb65c4e093dc
Piotr Szlaski <piotr.szlaski@ivanti.com>
2018-11-14 14:18:36 +0100

    finissh

--- ErrorHandlingExample/Program.cs
+++ ErrorHandlingExample/Program.cs
@@ -10,23 +10,41 @@ namespace ErrorHandlingExample
     {
         static async Task Main(string[] args)
         {
-            var block = new ActionBlock<int>(n =>
-            {
-                if (n == 5)
-                {
-                    throw new ArgumentException("Sth went wrong");
-                }
-                Console.WriteLine($"Message {n} processed");
-            });
+            var block = new TransformBlock<int, string>(n =>
+             {
+                 if (n == 5)
+                 {
+                     throw new ArgumentException("Sth went wrong");
+                 }
+                 Console.WriteLine($"Message {n} processed");
+                 return n.ToString();
+             });
+
+            var printBlock = new ActionBlock<string>(s =>
+                  Console.WriteLine($"block recived: {block.Receive()}")
+            );
+
+            block.LinkTo(printBlock, new DataflowLinkOptions(){PropagateCompletion = true});
 
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine(block.Post(i) ? $"Message {i} accepted" : $"Message {i} rejected");
             }
 
-            
-            Console.WriteLine($"Input queue size: {block.InputCount}");
-            await block.Completion;
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine($"block recived: {block.Receive()}");
+            //}
+            block.Complete();
+            try
+            {
+                await printBlock.Completion;
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine(e.Flatten().InnerExceptions);
+            }
+           
 
             Console.WriteLine("DONE");
             Console.ReadLine();
