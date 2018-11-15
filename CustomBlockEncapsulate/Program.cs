f132cbef08779d5a66e68d67a314de2ec6c23d0f
trendfollowerpl <trendfollowerpl@windowslive.com>
2018-11-15 20:26:20 +0100

    CustomBlockEncapsulate finished

--- CustomBlockEncapsulate/Program.cs
+++ CustomBlockEncapsulate/Program.cs
@@ -9,19 +9,28 @@ namespace CustomBlockEncapsulate
     {
         static async Task Main(string[] args)
         {
+            var inputBlock = new TransformBlock<int, int>(n =>
+            {
+                if (n == 2)
+                {
+                    throw new Exception("exception thrown from inputBlock");
+                }
+                return n;
+            });
             var increasingBlock = CreateFilteringBlock<int>();
-
             var printBlock = new ActionBlock<int>(n => Console.WriteLine($"Message {n} recived"));
 
+            inputBlock.LinkToWithPropagation(increasingBlock);
             increasingBlock.LinkToWithPropagation(printBlock);
 
-            increasingBlock.Post(1);
-            increasingBlock.Post(2);
-            increasingBlock.Post(1);
-            increasingBlock.Post(3);
-            increasingBlock.Post(4);
-            increasingBlock.Post(2);
+            inputBlock.Post(1);
+            inputBlock.Post(2);
+            inputBlock.Post(1);
+            inputBlock.Post(3);
+            inputBlock.Post(4);
+            inputBlock.Post(2);
 
+            inputBlock.Complete();
             await printBlock.Completion;
 
             Console.WriteLine("done");
@@ -39,6 +48,17 @@ namespace CustomBlockEncapsulate
                 {
                     await source.SendAsync(item);
                     maxElement = item;
+                }
+            });
+            target.Completion.ContinueWith(a =>
+            {
+                if (a.IsFaulted)
+                {
+                    ((ITargetBlock<T>)source).Fault(a.Exception);
+                }
+                else
+                {
+                    source.Complete();
                 }
             });
 
