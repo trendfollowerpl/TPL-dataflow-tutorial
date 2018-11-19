642e76265add8b8764d7e7b09265bd7af6ff6c96
Piotr Szlaski <piotr.szlaski@ivanti.com>
2018-11-19 14:37:21 +0100

    Customblock.Inheritance initial code

--- Customblock.Inheritance/Program.cs
+++ Customblock.Inheritance/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using LinkToWithPropagation;
 
 namespace Customblock.Inheritance
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var broadcastBlock =
+                new BroadcastBlock<int>(a => a);
+
+            var a1 = new ActionBlock<int>(
+                a =>
+                {
+                    Console.WriteLine($"Message {a} was processed by consumer 1");
+                    Task.Delay(500).Wait();
+                }, new ExecutionDataflowBlockOptions() { BoundedCapacity = 1 });
+
+            var a2 = new ActionBlock<int>(
+                a =>
+                {
+                    Console.WriteLine($"Message {a} was processed by consumer 2");
+                    Task.Delay(150).Wait();
+                }, new ExecutionDataflowBlockOptions() { BoundedCapacity = 1 });
+
+            broadcastBlock.LinkToWithPropagation(a1);
+            broadcastBlock.LinkToWithPropagation(a2);
+
+            for (int i = 0; i < 10; i++)
+            {
+                await broadcastBlock.SendAsync(i);
+            }
+
+            broadcastBlock.Complete();
+            await a1.Completion;
+            await a2.Completion;
+
+            Console.WriteLine("done");
+            Console.ReadLine();
         }
     }
 }
