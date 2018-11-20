772a212f67e6ae3a3a8a5770e0e1f5fd90422d6f
trendfollowerpl <trendfollowerpl@windowslive.com>
2018-11-20 20:38:10 +0100

    example complete

--- GeneralPerformace/Program.cs
+++ GeneralPerformace/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace GeneralPerformace
 {
@@ -6,7 +10,54 @@ namespace GeneralPerformace
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sw = new Stopwatch();
+            const int ITERS = 6 * 1000 * 1000;
+            var are = new AutoResetEvent(false);
+
+            var ab = new ActionBlock<int>(i =>
+            {
+                if (i == ITERS)
+                {
+                    are.Set();
+                }
+            }, new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = ExecutionDataflowBlockOptions.Unbounded });
+
+            for (int j = 0; j < 10; j++)
+            {
+                sw.Restart();
+                for (int i = 0; i <= ITERS; i++)
+                {
+                    ab.Post(i);
+                }
+                are.WaitOne();
+                sw.Stop();
+                Console.WriteLine("Messages / sec: {0:N0}", ITERS / sw.Elapsed.TotalSeconds);
+            }
+
+
+            Console.WriteLine("Task");
+            for (int j = 0; j < 10; j++)
+            {
+                sw.Restart();
+
+                new TaskFactory().StartNew(() =>
+                {
+                    for (int i = 0; i <= ITERS; i++)
+                    {
+                        if (i == ITERS)
+                        {
+                            are.Set();
+                        }
+                    }
+                });
+                are.WaitOne();
+                sw.Stop();
+                Console.WriteLine("Messages / sec: {0:N0}", ITERS / sw.Elapsed.TotalSeconds);
+            }
+
+
+            Console.WriteLine("done");
+            Console.ReadLine();
         }
     }
 }
