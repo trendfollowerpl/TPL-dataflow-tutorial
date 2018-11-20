46573bdcc888dab1648016fcb18a285039fdbea1
trendfollowerpl <trendfollowerpl@windowslive.com>
2018-11-20 20:59:56 +0100

    there is a mistake in this code - check X!

--- GeneralPerformace/Program.cs
+++ GeneralPerformace/Program.cs
@@ -13,22 +13,35 @@ namespace GeneralPerformace
             var sw = new Stopwatch();
             const int ITERS = 6 * 1000 * 1000;
             var are = new AutoResetEvent(false);
-
+            int counter = 0;
+            int x;
             var ab = new ActionBlock<int>(i =>
             {
-                if (i == ITERS)
+                counter++;
+                x = i;
+                if (i >= ITERS)
                 {
                     are.Set();
                 }
-            }, new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = ExecutionDataflowBlockOptions.Unbounded });
+            }, new ExecutionDataflowBlockOptions { SingleProducerConstrained = false });
 
             for (int j = 0; j < 10; j++)
             {
                 sw.Restart();
-                for (int i = 0; i <= ITERS; i++)
+                Task[] tasks = new Task[6];
+
+                for (int k = 0; k < 6; k++)
                 {
-                    ab.Post(i);
+                    tasks[k] = new TaskFactory().StartNew(() =>
+                             {
+                                 for (int i = 0; i <= ITERS / 6; i++)
+                                 {
+                                     ab.Post(i);
+                                 }
+                             });
                 }
+
+                Task.WaitAll(tasks);
                 are.WaitOne();
                 sw.Stop();
                 Console.WriteLine("Messages / sec: {0:N0}", ITERS / sw.Elapsed.TotalSeconds);
