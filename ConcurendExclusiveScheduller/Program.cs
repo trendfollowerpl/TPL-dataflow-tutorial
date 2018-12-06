417085ed7f68839ac7cc1e2e67367c38022a3152
trendfollowerpl <trendfollowerpl@windowslive.com>
2018-12-06 09:30:14 +0100

    ConcurendExclusiveScheduller project added

---
+++ ConcurendExclusiveScheduller/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using LinkToWithPropagation;
+
+namespace ConcurendExclusiveScheduller
+{
+    class Program
+    {
+        static int shared = 0;
+        static async Task Main(string[] args)
+        {
+            var inputBlock = new BroadcastBlock<int>(a => a);
+            Action<int> actionBlockFunction = a =>
+            {
+                var random = new Random();
+                int counterValue = GetSharedObjectValue();
+                Task.Delay(random.Next(300)).Wait();
+                SetSharedObjectValue(counterValue + 1);
+            };
+
+            var incrementBlock1 = new ActionBlock<int>(actionBlockFunction);
+            var incrementBlock2 = new ActionBlock<int>(actionBlockFunction);
+
+            inputBlock.LinkToWithPropagation(incrementBlock1);
+            inputBlock.LinkToWithPropagation(incrementBlock2);
+
+            for (int i = 0; i < 10; i++)
+            {
+                inputBlock.Post(i);
+            }
+            inputBlock.Complete();
+            await incrementBlock1.Completion;
+            await incrementBlock2.Completion;
+
+            Console.WriteLine($"shared object vlaue: {GetSharedObjectValue()}");
+            Console.ReadLine();
+        }
+
+        private static void SetSharedObjectValue(int v)
+        {
+            shared = v;
+        }
+
+        private static int GetSharedObjectValue()
+        {
+            return shared;
+        }
+    }
+}
