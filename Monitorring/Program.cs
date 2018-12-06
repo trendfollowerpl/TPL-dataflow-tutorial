ac60f77a5bf3559e548ada4271c7ad3e17f2b1f5
trendfollowerpl <trendfollowerpl@windowslive.com>
2018-12-06 10:30:18 +0100

    Monitorring proj added

---
+++ Monitorring/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+using System.Threading.Tasks;
+using LinkToWithPropagation;
+
+namespace Monitorring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var broadcastBlock = new BroadcastBlock<int>(null);
+            var transformPositive = new TransformBlock<int, int>(x =>
+            {
+                Task.Delay(random.Next(200)).Wait();
+                return x;
+            });
+            var transformNegative = new TransformBlock<int, int>(x =>
+            {
+                Task.Delay(random.Next(300)).Wait();
+                return x * -1;
+            });
+            var joinBlock = new JoinBlock<int, int>();
+            var sumBlock = new ActionBlock<(int, int)>(tuple =>
+            {
+                Console.WriteLine($"{tuple.Item1}+{tuple.Item2}={tuple.Item1+tuple.Item2}");
+            });
+
+            broadcastBlock.LinkToWithPropagation(transformPositive);
+            broadcastBlock.LinkToWithPropagation(transformNegative);
+            transformNegative.LinkToWithPropagation(joinBlock.Target1);
+            transformPositive.LinkToWithPropagation(joinBlock.Target2);
+
+        }
+    }
+}
